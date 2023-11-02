12ca64b44d27631b8f5b5664396a932e7bd07439
Arjun S <arjun8107@gmail.com>
2023-11-03 00:15:32 +0530

    Cool hero effect

--- src/components/GridAnimations.tsx
+++ src/components/GridAnimations.tsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react";
 
 const GridAnimations = () => {
-  const [columns, setColumns] = useState(3);
-  const [rows, setRows] = useState(3);
-
   useEffect(() => {
-    const handleResize = () => {
-      setColumns(window.innerWidth / 100);
-      setRows(window.innerHeight / 100);
+    // use animate to update background image position css to mouse position
+    const animate = () => {
+      const grid = document.querySelector(".autogrid") as HTMLElement;
+      if (grid) {
+        const { x, y } = grid.getBoundingClientRect();
+        document.addEventListener("mousemove", (e) => {
+          const { clientX, clientY } = e;
+          const xDiff = clientX - x;
+          const yDiff = clientY - y;
+          grid.style.backgroundPosition = `${xDiff / 3}px ${yDiff / 3}px`;
+        });
+      }
+      requestAnimationFrame(animate);
     };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
+    requestAnimationFrame(animate);
   }, []);
-
-  return <div className="w-screeen absolute z-10 h-screen bg-foreground"></div>;
+  return (
+    <div
+      className={`autogrid absolute left-1/2 top-1/2 -z-10 grid aspect-square h-screen w-full -translate-x-1/2 -translate-y-1/2  overflow-hidden opacity-90`}
+    >
+      {Array.from(Array(810).keys()).map((i) => (
+        <div key={i} className="mb-px mr-px bg-background" />
+      ))}
+    </div>
+  );
 };
 
 export default GridAnimations;
