089d909645d63ee4062584cf0ba985e863c6e178
Arjun S <arjun8107@gmail.com>
2023-11-02 16:53:18 +0530

    Responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,18 @@ import {
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { ModeToggle } from "./ModeToggle";
 import { PaintBucketIcon } from "lucide-react";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
 
 const components: { title: string; href: string }[] = [
   {
@@ -44,24 +54,45 @@ export function Navbar() {
     <header className="mx-auto flex max-w-7xl items-center justify-between p-8">
       <PaintBucketIcon />
       <NavigationMenu>
-        <NavigationMenuList>
-          {components.map((component) => (
-            <NavigationMenuItem key={component.title + component.href}>
-              <a href={component.href}>
-                <NavigationMenuLink
-                  className={navigationMenuTriggerStyle({
-                    className:
-                      "bg-transparent text-gray-900 dark:text-gray-100",
-                  })}
-                >
-                  {component.title}
-                </NavigationMenuLink>
-              </a>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
+        <NavList className="hidden md:flex" />
+        <Sheet>
+          <SheetTrigger className="md:hidden">
+            <Button variant="outline" size="icon">
+              <HamburgerMenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <NavList className="mt-14 flex-col" textSize="large" />
+          </SheetContent>
+        </Sheet>
         <ModeToggle />
       </NavigationMenu>
     </header>
   );
 }
+
+const NavList = (props: {
+  className?: string;
+  textSize?: "large" | "normal";
+}) => (
+  <NavigationMenuList className={props.className}>
+    {components.map((component) => (
+      <NavigationMenuItem
+        key={component.title + component.href}
+        className="self-start"
+      >
+        <a href={component.href}>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle({
+              className: `bg-transparent ${
+                props.textSize == "large" ? "text-xl" : ""
+              } text-gray-900 dark:text-gray-100`,
+            })}
+          >
+            {component.title}
+          </NavigationMenuLink>
+        </a>
+      </NavigationMenuItem>
+    ))}
+  </NavigationMenuList>
+);
