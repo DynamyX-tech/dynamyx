ccf41a3f882a38af01390baf608bc436f34cfa95
Arjun S <arjun8107@gmail.com>
2023-11-06 22:41:21 +0530

    precio landing start

--- src/components/precio/Navbar.tsx
+++ src/components/precio/Navbar.tsx
@@ -6,10 +6,9 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { ModeToggle } from "./ModeToggle";
-import { PaintBucketIcon } from "lucide-react";
+import { ModeToggle } from "../ModeToggle";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 import Logo from "./Logo";
 
 const components: { title: string; href: string }[] = [
@@ -33,29 +32,30 @@ const components: { title: string; href: string }[] = [
 
 export function Navbar() {
   return (
-    <header className="nav container mx-auto flex items-center justify-between p-2">
-      <div className="flex items-center gap-4">
-        <Logo viewBox="0 0 500 70" height={70} width={140} />
-      </div>
-      <NavigationMenu>
-        <NavList className="hidden md:flex" />
-        <Sheet>
-          <SheetTrigger className="md:hidden">
-            <Button
-              variant="outline"
-              size="icon"
-              aria-label="open navigation sidebar"
-            >
-              <HamburgerMenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SheetContent>
-            <NavList className="mt-14 flex-col" textSize="large" />
-          </SheetContent>
-        </Sheet>
-        <ModeToggle />
-      </NavigationMenu>
-    </header>
+    <div className="w-full bg-background text-foreground">
+      <header className="container mx-auto flex items-center justify-between p-2">
+        <div className="flex items-center gap-4">
+          <Logo viewBox="0 0 500 70" />
+        </div>
+        <NavigationMenu>
+          <NavList className="hidden md:flex" />
+          <Sheet>
+            <SheetTrigger className="md:hidden">
+              <Button
+                variant="outline"
+                size="icon"
+                aria-label="open navigation sidebar"
+              >
+                <HamburgerMenuIcon />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <NavList className="mt-14 flex-col" textSize="large" />
+            </SheetContent>
+          </Sheet>
+        </NavigationMenu>
+      </header>
+    </div>
   );
 }
 
@@ -74,7 +74,7 @@ const NavList = (props: {
           className={navigationMenuTriggerStyle({
             className: `bg-transparent ${
               props.textSize == "large" ? "text-xl" : ""
-            } text-gray-900 dark:text-gray-100`,
+            } text-foreground`,
           })}
         >
           {component.title}
