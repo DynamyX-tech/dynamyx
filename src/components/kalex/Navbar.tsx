814c37e846e6367e40ac4a4a8ef9ced7380f56f2
a-run-kumar-d <arunkumarpkmn@example.com>
2023-12-22 23:52:23 +0530

    navbar + servicenav functionality

--- src/components/kalex/Navbar.tsx
+++ src/components/kalex/Navbar.tsx
@@ -1,7 +1,17 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
 export default function Navbar(){
     return(
-        <div>
-            
+        <div className="w-full flex flex-row justify-between items-center">
+            <img src="/public/Kalexlogo.png" className="h-12"></img>
+            <div className="md:flex flex-row gap-8 hidden">
+                <a href="/kalex#usecases">Use cases</a>
+                <a href="/kalex#feature">Features</a>
+                <a href="/kalex#services">Services</a>
+            </div>
+            <button className=" w-36 h-12 border border-foreground rounded-full md:flex flex-row justify-center items-center hidden ">Contact us</button>
+            <button className="md:hidden border p-2 rounded">
+                <HamburgerMenuIcon/></button>
         </div>
     )
 }
--- src/components/kalex/Servicenav.tsx
+++ src/components/kalex/Servicenav.tsx
@@ -1,5 +1,28 @@
 import { useState } from "react";
 
+type Data = {
+  [key: string]: {
+    title: string;
+    description: string;
+    image: string;
+  };
+};
+
+const data: Data = {
+  mobileapp: {
+    title: "Mobile App",
+    description:
+      "Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit autfugit, sed quia consequuntur magni dolores eos, qui ratione voluptatemsequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quiadolor sit amet consectetur adipisci[ng] velit, sed quia non numquam Nemo enim iput odit aut fugit, sed quia consequuntur magni doloreseos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est,qui dolorem ipsum, quia Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt,",
+    image: "/public/kalexMobile.png",
+  },
+  dashboard: {
+    title: "Dashboard",
+    description:
+      "sectetur adipisci[ng] velit, sed quia non numquam Nemo enim iput odit aut fugit, sed quia consequuntur magni doloreseos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est,qui dolorem ipsum, quia Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt",
+    image: "/public/kalexDashboard.png",
+  },
+};
+
 const Servicenav = () => {
   const [selected, setSelected] = useState("mobileapp");
   return (
@@ -32,19 +55,13 @@ const Servicenav = () => {
           </button>
         </div>
         <h4 className="md:w-96 text-xl text-foreground">
-          Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut
+          {data[selected].title}
         </h4>
         <p className="md:w-[480px]">
-          Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut
-          fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem
-          sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia
-          dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam
-          Nemo enim iput odit aut fugit, sed quia consequuntur magni dolores
-          eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est,
-          qui dolorem ipsum, quia
+          {data[selected].description}
         </p>
       </div>
-      <img src="/public/kalexMobile.png" className="mt-10 md:-mt-36 h-[400px]"></img>
+      <img src={data[selected].image} className="mt-10 md:-mt-36 h-[400px] max-w-96"></img>
     </div>
   );
 };
