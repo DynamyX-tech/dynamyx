4b96a03fb77a9bb4e2221d7f11d3ba994be89fa9
a-run-kumar-d <arunkumarpkmn@example.com>
2024-01-17 22:47:20 +0530

    Refactor Navbar component and update FAQ section

--- src/components/kalex/Navbar.tsx
+++ src/components/kalex/Navbar.tsx
@@ -1,16 +1,20 @@
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export default function Navbar(){
+    const [open, setOpen] = useState(false);
     return(
         <div className="w-full flex flex-row justify-between items-center">
             <img src="/public/Kalexlogo.png" className="h-12"></img>
             <div className="md:flex flex-row gap-8 hidden">
                 <a href="/kalex#usecases">Use cases</a>
-                <a href="/kalex#feature">Features</a>
                 <a href="/kalex#services">Services</a>
+                <a href="/kalex#faq">FAQ</a>
             </div>
             <button className=" w-36 h-12 border border-foreground rounded-full md:flex flex-row justify-center items-center hidden ">Contact us</button>
-            <button className="md:hidden border p-2 rounded">
+            <button className="md:hidden border p-2 rounded" onClick={()=>{
+                setOpen(!open);
+            }}>
                 <HamburgerMenuIcon/></button>
         </div>
     )
